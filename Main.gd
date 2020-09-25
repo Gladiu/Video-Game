e1414277bd3e71cfd56c5286937dd56a1071c146
Gladiu <46225267+Gladiu@users.noreply.github.com>
2020-09-25 20:04:44 +0200

    Cleanup and adding proper camera debug movement with arrows

--- Main.gd
+++ Main.gd
@@ -1,16 +1,40 @@
 extends Node
 
+var dungeon_scene
+var dungeon_node
+var hero_scene
+var hero_node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# Loading Dungeon scene
+	dungeon_scene = preload("res://Dungeon.tscn")
+	dungeon_node = dungeon_scene.instance()
+	add_child(dungeon_node)
+	
+	# Loading Hero scene
+	hero_scene = preload("res://Hero.tscn")
+	hero_node = dungeon_scene.instance()
+	add_child(hero_node)
+	
+	# Setting up camera position
+	var camera_transform = $Camera.get_camera_transform()
+	camera_transform.origin.y += 30
+	$Camera.transform = camera_transform
+	
+func _process(delta):
+	var movement = Vector2(0,0)
+	if Input.is_action_just_pressed("ui_up"):
+		movement = Vector2(-1,-1)
+	if Input.is_action_just_pressed("ui_down"):
+		movement = Vector2(1,1)
+	if Input.is_action_just_pressed("ui_left"):
+		movement = Vector2(-1,1)
+	if Input.is_action_just_pressed("ui_right"):
+		movement = Vector2(1,-1)
+	if movement.x != 0 or movement.y != 0:
+		$Camera.transform.origin.x += movement.normalized().x
+		$Camera.transform.origin.z += movement.normalized().y
